9126766e4c6855146bbd3da90c5bfdc479a04e7e
artem.puhach <artem.puhach@cyberpolice.gov.ua>
2025-02-06 16:51:26 +0200

    ready solution!

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM python:alpine3.21
+LABEL maintainer="job.artem.2019@gmail.com"
+
+ENV PYTHONBUFFERED 1
+
+WORKDIR app/
+
+COPY requirements.txt requirements.txt
+RUN pip install -r requirements.txt
+
+COPY . .
+
+CMD ["python", "app/main.py"]
--- app/main.py
+++ app/main.py
@@ -1,16 +1,17 @@
+import os
 import requests
 
-API_KEY = "2aba40a7f9aa4bfaa97123256250602"
+# Читаем API_KEY из переменных окружения
+API_KEY = os.getenv("API_KEY")
+if not API_KEY:
+    raise ValueError("API_KEY is not set. Please set it as an environment variable.")
+
 BASE_URL = "http://api.weatherapi.com/v1/current.json"
 
 def get_weather() -> None:
-    print(f"Performing request to Weather API for city Paris...")
+    print("Performing request to Weather API for city Paris...")
 
-    params = {
-        "key": API_KEY,
-        "q": "Paris",
-        "aqi": "no"
-    }
+    params = {"key": API_KEY, "q": "Paris", "aqi": "no"}
 
     try:
         response = requests.get(BASE_URL, params=params)
@@ -21,12 +22,16 @@ def get_weather() -> None:
         country = data.get("location", {}).get("country", "Unknown country")
         localtime = data.get("location", {}).get("localtime", "Unknown time")
         temp_c = data.get("current", {}).get("temp_c", "N/A")
-        condition = data.get("current", {}).get("condition", {}).get("text", "Unknown condition")
+        condition = (
+            data.get("current", {}).get("condition", {}).get("text", "Unknown condition")
+        )
 
-        print(f"{location}/{country} {localtime} Weather: {temp_c} Celsius, {condition}")
+        print(
+            f"{location}/{country} {localtime} "
+            f"Weather: {temp_c} Celsius, {condition}"
+        )
     except requests.exceptions.RequestException as e:
         print(f"Error fetching weather data: {e}")
-
 
 if __name__ == "__main__":
     get_weather()
